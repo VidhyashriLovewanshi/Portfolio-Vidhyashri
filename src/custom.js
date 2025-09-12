d2927ef463f8d3a554b259dda3eaf7877f2757f3
Vidhyashri-Lovewanshi <vjlshri8689@gmail.com>
2025-09-12 19:57:19 +0530

    changes1

---
+++ src/custom.js
@@ -0,0 +1,49 @@
+ 
+     AOS.init({
+      duration: 1200, delay: 200
+    })
+     
+     var typed = new Typed("#typing", {
+      strings: ["Frontend Developer", "React Frontend Developer", "Creative Coder"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
+
+    // Scroll-to-top
+    const scrollBtn = document.getElementById('scrollTopBtn');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 300) {
+        scrollBtn.classList.remove('hidden');
+      } else {
+        scrollBtn.classList.add('hidden');
+      }
+    });
+    scrollBtn.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+ 
+ const openBtns = document.querySelectorAll('.open-about'); // sabhi buttons
+  const aboutPopup = document.getElementById('aboutPopup');
+  const popupContent = document.getElementById('popupContent');
+  const closeBtn = document.getElementById('closePopup');
+
+  // Open Popup
+  openBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      aboutPopup.classList.remove('hidden');
+      setTimeout(() => {
+        popupContent.classList.remove('scale-95', 'opacity-0');
+        popupContent.classList.add('scale-100', 'opacity-100');
+      }, 10); // chhota delay transition ke liye
+    });
+  });
+
+  // Close Popup
+  closeBtn.addEventListener('click', () => {
+    popupContent.classList.remove('scale-100', 'opacity-100');
+    popupContent.classList.add('scale-95', 'opacity-0');
+    setTimeout(() => {
+      aboutPopup.classList.add('hidden');
+    }, 300); // same as transition duration
+  });
